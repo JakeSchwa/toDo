0186eae9d9f7df69ddbf5db803bd2dabef274a98
Jake Schwarztrauber <jake@Jakes-MacBook-Pro.local>
2020-03-01 12:41:06 -0500

    setting up repo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {useEffect} from 'react';
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
+
+
+const App = () => {
+  useEffect(()=>{
+    // Init Materialize JS
+    M.AutoInit();
+  })
+  return (
+    <div className="App">
+      My Application
+    </div>
+  );
+}
+
+export default App;
