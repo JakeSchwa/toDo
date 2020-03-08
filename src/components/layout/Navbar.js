22567985772571cf1324d40b62e87af515d597c9
Jake Schwarztrauber <jakeschwa@gmail.com>
2020-03-08 18:20:23 -0400

    made nav component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="!#" className="brand-logo" id="title">Logo</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
