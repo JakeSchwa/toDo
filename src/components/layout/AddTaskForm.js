30294dfc01e879f9162fa2a391ed1413a7e4b834
Jake Schwarztrauber <jakeschwa@gmail.com>
2020-04-01 19:44:15 -0400

    added toast to Add Task form

--- src/components/layout/AddTaskForm.js
+++ src/components/layout/AddTaskForm.js
@@ -6,15 +6,37 @@ import { addTask } from '../../actions/taskActions'
 import PropTypes from 'prop-types'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import Snackbar from '@material-ui/core/Snackbar'
 
 const AddTaskForm = ({ addTask }) => {
   const [description, setDescription] = useState('')
   const [error, setError] = useState(false)
   const [helperText, setHelperText] = useState(' ')
+  const [showSnack, setShowSnack] = useState(false)
+
+  const handleClose = () => {
+    setShowSnack(false)
+  }
+
+  const displaySnackBar = () => {
+    return (
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={showSnack}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Task Added"
+      />
+    )
+  }
 
   const handleSubmit = event => {
     if (taskIsValid()) {
       postTask()
+      setShowSnack(true)
     } else {
       showError(true)
     }
@@ -55,30 +77,33 @@ const AddTaskForm = ({ addTask }) => {
   }
 
   return (
-    <Card id="add-task-card">
-      <CardContent>
-        <form onSubmit={handleSubmit} id="add-task-form">
-          <TextField
-            id="add-task-input"
-            placeholder="Task Description"
-            onChange={handleChange}
-            error={error}
-            value={description}
-            helperText={helperText}
-            autoComplete="off"
-            fullWidth
-          />
-          <Button
-            id="add-task-submit-button"
-            variant="contained"
-            color="primary"
-            type="submit"
-          >
-            Add Task
-          </Button>
-        </form>
-      </CardContent>
-    </Card>
+    <>
+      {displaySnackBar()}
+      <Card id="add-task-card">
+        <CardContent>
+          <form onSubmit={handleSubmit} id="add-task-form">
+            <TextField
+              id="add-task-input"
+              placeholder="Task Description"
+              onChange={handleChange}
+              error={error}
+              value={description}
+              helperText={helperText}
+              autoComplete="off"
+              fullWidth
+            />
+            <Button
+              id="add-task-submit-button"
+              variant="contained"
+              color="primary"
+              type="submit"
+            >
+              Add Task
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </>
   )
 }
 
